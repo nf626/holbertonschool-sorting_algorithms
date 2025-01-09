820344a9504ce5b0c11b896c6c1bd051c568f49f
Nigel Feng <10083@holbertonstudents.com>
2025-01-09 12:26:18 +0000

    0. Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,16 +2,16 @@
 #include <stdbool.h>
 
 /**
- * swap - function to swap the x and y. 
+ * swap - function to swap the x and y.
  * @x: first value.
  * @y: second value.
  *
  */
-void swap(int* x, int* y)
+void swap(int *x, int *y)
 {
-  int temp = *x;
-  *x = *y;
-  *y = temp;
+int temp = *x;
+*x = *y;
+*y = temp;
 }
 /**
  * bubble_sort - function that sorts an array of integers in ascending order
@@ -22,32 +22,32 @@ void swap(int* x, int* y)
  */
 void bubble_sort(int *array, size_t size)
 {
-  size_t i = 0, j;
-  bool swapped;
+size_t i = 0, j;
+bool swapped;
 
-  if (array == NULL || size < 2)
-    {
-      exit(1);
-    }
-  
-  while (i < size - 1)
-    {
-      j = 0;
-      swapped = false;
-      while (j < size - i - 1)
-	{
-	  if (array[j] > array[j + 1])
-	    {
-	      swap(&array[j], &array[j + 1]);
-	      print_array(array, size);
-	      swapped = true;
-	    }
-	  j = j + 1;
-	}
-      if (swapped == false)
-	{
-	  break;
-	}
-      i = i + 1;
-    }
+if (array == NULL || size < 2)
+{
+exit(EXIT_FAILURE);
+}
+
+while (i < size - 1)
+{
+j = 0;
+swapped = false;
+while (j < size - i - 1)
+{
+if (array[j] > array[j + 1])
+{
+swap(&array[j], &array[j + 1]);
+print_array(array, size);
+swapped = true;
+}
+j = j + 1;
+}
+if (swapped == false)
+{
+break;
+}
+i = i + 1;
+}
 }
