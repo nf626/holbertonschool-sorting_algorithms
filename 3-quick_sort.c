abcc1a032057e20958b1aeb5510ea458ffa658a1
Nigel Feng <10083@holbertonstudents.com>
2025-01-10 05:58:33 +0000

    3. Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,55 +13,58 @@ int temp = *x;
 *y = temp;
 }
 /**
- * lomuto_part
+ * lomuto_part - order array to two sub-arrays and perform partition.
+ * @array: pointer to an array of integers.
+ * @size: size of array.
+ * @left: first index in sub-array part.
+ * @right: last index in sub-array part.
  *
- *
- *
+ * Return: partition index.
  */
 int lomuto_part(int *array, size_t size, int left, int right)
 {
-  int *pivot, low, high;
+int *pivot, low, high;
 
-  pivot = array + right;
-  high = low = left;
-  while (low < right)
-    { 
-      if (array[low] < *pivot)
-	{
-	  if (high < low)
-	    {
-	      swap(array + low, array + high);
-	      print_array(array, size);
-	    }
-	  high = high + 1;
-	}
-      low = low + 1;
-    }
-  if (array[high] > *pivot)
-    {
-      swap(array + high, pivot);
-      print_array(array, size);
-    }
-  return (high);
+pivot = array + right;
+high = low = left;
+while (low < right)
+{
+if (array[low] < *pivot)
+{
+if (high < low)
+{
+swap(array + low, array + high);
+print_array(array, size);
+}
+high = high + 1;
+}
+low = low + 1;
+}
+if (array[high] > *pivot)
+{
+swap(array + high, pivot);
+print_array(array, size);
+}
+return (high);
 }
 /**
  * lomuto_sort - function that uses quicksort through recursion.
  * @array: pointer to an array of integers.
  * @size: size of array.
- * @low: first index of array.
- * @high: last index of array. 
+ * @left: first index of array.
+ * @right: last index of array.
  *
  */
 void lomuto_sort(int *array, size_t size, int left, int right)
 {
-  int partition;
+int partition;
 
-  if (right - left > 0)
-    {
-      partition = lomuto_part(array, size, left, right);
-      lomuto_sort(array, size, left, partition - 1);
-      lomuto_sort(array, size, partition + 1, right);
-    }
+if (right - left > 0)
+{
+partition = lomuto_part(array, size, left, right);
+lomuto_sort(array, size, left, partition - 1);
+lomuto_sort(array, size, partition + 1, right);
+}
 }
 /**
  * quick_sort - function that sorts an array of integers in
@@ -72,10 +75,10 @@ void lomuto_sort(int *array, size_t size, int left, int right)
  */
 void quick_sort(int *array, size_t size)
 {
-  if (array == NULL || size < 2)
-    {
-      return;
-    }
+if (array == NULL || size < 2)
+{
+return;
+}
 
-  lomuto_sort(array, size, 0, size - 1); 
+lomuto_sort(array, size, 0, size - 1);
 }
