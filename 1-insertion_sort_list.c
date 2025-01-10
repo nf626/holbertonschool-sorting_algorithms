6ee7ffcdb89c1e0401712bd40c537b841e59be57
Nigel Feng <10083@holbertonstudents.com>
2025-01-10 02:10:25 +0000

    1. Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,50 +9,51 @@
  */
 void swap_node(listint_t **head, listint_t **n1, listint_t *n2)
 {
-  (*n1)->next = n2->next;
-  if (n2->next != NULL)
-    {
-      n2->next->prev = *n1;
-    }
-  n2->prev = (*n1)->prev;
-  n2->next = *n1;
-  if ((*n1)->prev != NULL)
-    {
-      (*n1)->prev->next = n2;
-    }
-  else
-    *head = n2;
-  (*n1)->prev = n2;
-  *n1 = n2->prev;
+(*n1)->next = n2->next;
+if (n2->next != NULL)
+{
+n2->next->prev = *n1;
+}
+n2->prev = (*n1)->prev;
+n2->next = *n1;
+if ((*n1)->prev != NULL)
+{
+(*n1)->prev->next = n2;
+}
+else
+*head = n2;
+(*n1)->prev = n2;
+*n1 = n2->prev;
 }
 /**
  * insertion_sort_list - function that sorts a doubly linked list of
- *                       integers in ascending order using the Insertion sort algorithm.
+ *                       integers in ascending order using the
+ *                       Insertion sort algorithm.
  * @list: double pointer to a list.
  *
  */
 void insertion_sort_list(listint_t **list)
 {
-  listint_t *temp, *iterate = (*list)->next, *insert;
-  
-  if (list == NULL || *list == NULL || (*list)->next == NULL)
-    {
-      return;
-    }
+listint_t *temp, *iterate = (*list)->next, *insert;
 
-  /** traverse list */
-  while (iterate != NULL)
-    {
-      /** store next value */
-      temp = iterate->next;
-      /** store previous value */
-      insert = iterate->prev;
-      
-      while (insert != NULL && iterate->n < insert->n)
-	{
-	  swap_node(list, &insert, iterate);
-	  print_list((const listint_t *)*list);
-	}
-      iterate = temp;
-    }
+if (list == NULL || *list == NULL || (*list)->next == NULL)
+{
+return;
+}
+
+/** traverse list */
+while (iterate != NULL)
+{
+/** store next value */
+temp = iterate->next;
+/** store previous value */
+insert = iterate->prev;
+
+while (insert != NULL && iterate->n < insert->n)
+{
+swap_node(list, &insert, iterate);
+print_list((const listint_t *)*list);
+}
+iterate = temp;
+}
 }
